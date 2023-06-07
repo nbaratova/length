e1e518b9fddecb8cf707a26617c9d217ee253c87
Zufar <zufarbekz@bk.ru>
2023-06-07 06:56:56 +0000

    test

--- main.py
+++ main.py
@@ -9,12 +9,13 @@ import wikipedia
 from aiogram import Bot, Dispatcher, executor, types
 
 API_TOKEN = '6149940727:AAEe4Yem7SMs1w2gBYurVO7lm8lE5iJSq8E'
+PROXY_URL = "http://proxy.server:3128"
 
 # Configure logging
 logging.basicConfig(level=logging.INFO)
 
 # Initialize bot and dispatcher
-bot = Bot(token=API_TOKEN)
+bot = Bot(token=API_TOKEN, proxy=PROXY_URL)
 dp = Dispatcher(bot)
 wikipedia.set_lang('uz')
 
